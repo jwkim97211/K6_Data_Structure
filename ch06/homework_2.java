313bdb30278b7687dd4d9e62e2ff5b59e9164adf
jwkim97211 <160681245+jwkim97211@users.noreply.github.com>
2024-03-26 18:01:25 +0900

    Add files via upload

--- ch06/homework_2.java
+++ ch06/homework_2.java
@@ -1,40 +1,49 @@
 package ch06;
 
-class Polynomial implements Comparable<Polynomial> {
+import java.text.DecimalFormat;
+
+/*
+ * 6장 구현과제1
+ */
+
+class Polynomial3 implements Comparable<Polynomial3> {
 	double coef; // 계수
 	int exp; // 지수
 
-	public Polynomial(double coef, int exp) {
+	Polynomial3() {
+	}
+
+	// --- 생성자(constructor) ---//
+	Polynomial3(double coef, int exp) {
 		this.coef = coef;
 		this.exp = exp;
 	}
 
+	// --- 신체검사 데이터를 문자열로 반환 --//
 	@Override
-	public int compareTo(Polynomial o) {
-		if (this.exp > o.exp)
-			return 1;
-		else if (this.exp < o.exp)
-			return -1;
-		else
-			return 0;
+	public String toString() {
+		return coef + "x**" + exp;
 	}
 
 	@Override
-	public String toString() {
-		return coef + "X^" + exp;
+	public int compareTo(Polynomial3 d2) { // 지수가 같으면 계수로 비교
+		return d2.exp - exp;
 	}
 }
 
 public class homework_2 {
 
-	static void merge(Polynomial[] a, int lefta, int righta, int leftb, int rightb) {
-		Polynomial[] temp = new Polynomial[a.length];
+	// --- 배열 요소 a[idx1]와 a[idx2]의 값을 교환 ---//
+	static void merge(Polynomial3[] a, int lefta, int righta, int leftb, int rightb) {
+		// body를 지우고 작성 훈련 연습이 도움이 된다
+		Polynomial3 temp[] = new Polynomial3[30];
+		// 구현코드
 		int idx = 0;
 		int p = lefta, q = leftb;
 		while (p <= righta && q <= rightb) {
-			if (a[p].compareTo(a[q]) == -1)
+			if (a[p].compareTo(a[q]) > 0)
 				temp[idx++] = a[p++];
-			else if (a[p].compareTo(a[q]) == 1)
+			else if (a[p].compareTo(a[q]) < 0)
 				temp[idx++] = a[q++];
 			else {
 				temp[idx++] = a[p++];
@@ -50,7 +59,7 @@ public class homework_2 {
 	}
 
 	// --- 퀵 정렬(비재귀 버전)---//
-	static void MergeSort(Polynomial[] a, int left, int right) {
+	static void MergeSort(Polynomial3[] a, int left, int right) {
 		int mid = (left + right) / 2;
 		if (left == right)
 			return;
@@ -60,110 +69,140 @@ public class homework_2 {
 		return;
 	}
 
-	public static void main(String[] args) {
-		Polynomial[] x = { new Polynomial(1.5, 3), 
-						   new Polynomial(2.5, 7), 
-						   new Polynomial(3.3, 2),
-					 	   new Polynomial(4.0, 1), 
-						   new Polynomial(2.2, 0), 
-						   new Polynomial(3.1, 4), 
-						   new Polynomial(3.8, 5), 
-						   };
-		Polynomial[] y = { new Polynomial(1.5, 1), 
-				           new Polynomial(2.5, 2), 
-						   new Polynomial(3.3, 3),
-						   new Polynomial(4.0, 0), 
-						   new Polynomial(2.2, 4), 
-						   new Polynomial(3.1, 5), 
-						   new Polynomial(3.8, 6), 
-						   };
-		int nx = x.length;
-		
-		System.out.println("x");
-		ShowPolynomial(x);
-		System.out.println("y");
-		ShowPolynomial(y);
-		MergeSort(x, 0, x.length - 1); // 배열 x를 퀵정렬
-		MergeSort(y, 0, y.length - 1); // 배열 x를 퀵정렬
-		System.out.println("x");
-		ShowPolynomial(x);
-		System.out.println("y");
-		ShowPolynomial(y);
-		Polynomial[] z = new Polynomial[8];
-		AddPolynomial(x, y, z);// 다항식 덧셈 z = x + y
-		System.out.println("x+y");
-		ShowPolynomial(z);
-		z = new Polynomial[14];
-		MultiplyPolynomial(x, y, z);// 다항식 곱셈 z = x * y
-		System.out.println("x*y");
-		ShowPolynomial(z);
-		double result = EvaluatePolynomial(z, 10);// 다항식 값 계산 함수 z(10) 값 계산한다
-		System.out.println(" result = " + result);
+	static void ShowPolynomial(String str, Polynomial3[] x, int count) {
+		// str 변수는 다항식 이름으로 스트링이다
+		// count가 -1이면 다항식 x의 length로 계산하고 양수이면 count가 다항식 항의 숫자이다
+		// 정렬후 다항식 x = 2.5x**7 + 3.8x**5 + 3.1x**4 + 1.5x**3 + 3.3x**2 + 4.0x**1 +
+		// 2.2x**0
+		int n = 0;
+		if (count < 0)
+			n = x.length;
+		else
+			n = count;
+		// 구현코드
+		System.out.print(str);
+		for (int i = 0; i < n; i++)
+			if (i != n - 1)
+				System.out.printf("%.2f x ** %d + ", x[i].coef, x[i].exp);
+			else
+				System.out.printf("%.2f x ** %d\n", x[i].coef, x[i].exp);
 	}
 
-	static double EvaluatePolynomial(Polynomial[] z, int i) {
-		double result=0;
-		for(int j=0;j<z.length;j++)
-			result+=z[j].coef*Math.pow(i, z[j].exp);
+	static int AddPolynomial(Polynomial3[] x, Polynomial3[] y, Polynomial3[] z) {
+		// z = x + y, 다항식 덧셈 결과를 z로 주고 z의 항의 수 terms을 리턴한다
+		int p = 0, q = 0, r = 0;
+		int terms = 0;
+		// 구현코드
+		while (p < x.length && q < y.length) {
+			if (x[p].exp == y[q].exp) {
+				z[r++] = new Polynomial3(x[p].coef + y[q].coef, x[p].exp);
+				p++;
+				q++;
+				terms++;
+			} else if (x[p].exp < y[q].exp) {
+				z[r++] = new Polynomial3(x[p].coef, x[p].exp);
+				p++;
+				terms++;
+			} else {
+				z[r++] = new Polynomial3(y[q].coef, y[q].exp);
+				q++;
+				terms++;
+			}
+		}
+
+		while (p < x.length) {
+			z[r++] = new Polynomial3(x[p].coef, x[p].exp);
+			p++;
+			terms++;
+		}
+
+		while (q < y.length) {
+			z[r++] = new Polynomial3(y[q].coef, y[q].exp);
+			q++;
+			terms++;
+		}
+		return terms;
+	}
+
+	static int addTerm(Polynomial3[] z, Polynomial3 term, int terms) {
+		// 다항식 z에 새로운 항 term을 추가한다. 지수가 같은 항이 있으면 계수만 합한다
+		// 추가된 항의 수를 count하여 terms으로 리턴한다.
+		// 구현코드
+		for (int i = 0; i < terms; i++) {
+			if (z[i].exp == term.exp) {
+				z[i].coef += term.coef;
+				return terms;
+			}
+		}
+		z[terms] = term;
+		return terms+1;
+	}
+
+	static int MultiplyPolynomial(Polynomial3[] x, Polynomial3[] y, Polynomial3[] z) {
+		// z = x * y, 다항식 z의 항의 수는 terms으로 리턴한다
+		// terms = addTerm(z, term, terms);사용하여 곱셈항을 추가한다.
+		int p = 0, q = 0, r = 0;
+		int terms = 0;
+		// 구현코드
+		for (p = 0; p < x.length; p++) {
+			for (q = 0; q < y.length; q++) {
+				Polynomial3 newTerm = new Polynomial3(x[p].coef * y[q].coef,x[p].exp + y[q].exp);
+				terms = addTerm(z, newTerm, terms);
+			}
+		}
+		return terms;
+	}
+
+	static double EvaluatePolynomial(Polynomial3[] z, int zTerms, int value) {
+		// zTerms는 다항식 z의 항의 수, value는 f(x)를 계산하기 위한 x 값
+		// 다항식 계산 결과를 double로 리턴한다
+		double result = 0.0;
+		// 구현 코드
+		for (int j = 0; j < zTerms; j++)
+			result += z[j].coef * Math.pow(value, z[j].exp);
 		return result;
 	}
 
-	static void MultiplyPolynomial(Polynomial[] x, Polynomial[] y, Polynomial[] z) {
-		int idx=0;
-		Polynomial[] z1 = new Polynomial[x.length * y.length];
-		for(int i=0;i<x.length;i++) {
-			for(int j=0;j<y.length;j++) {
-				z1[idx++]=new Polynomial(x[i].coef * y[j].coef, x[i].exp+y[j].exp);
-			}
-		}
-		MergeSort(z1, 0, idx - 1);
-	    for (int i = 0; i < idx - 1; i++) {
-	        if (z1[i].exp == z1[i + 1].exp) {
-	            z1[i].coef += z1[i + 1].coef;
-	            for (int j = i + 1; j < idx - 1; j++) {
-	                z1[j] = z1[j + 1];
-	            }
-	            idx--;
-	            i--;
-	            z1[idx]=null;
-	        }
-	    }
-	    for(int i=0;i<z.length;i++)
-	    	z[i]=z1[i];
-	}
+	public static void main(String[] args) {
+		Polynomial3[] x = { new Polynomial3(1.5, 3),
+							new Polynomial3(2.5, 7), 
+							new Polynomial3(3.3, 2),
+							new Polynomial3(4.0, 1), 
+							new Polynomial3(2.2, 0), 
+							new Polynomial3(3.1, 4), 
+							new Polynomial3(3.8, 5), 
+							};
+		Polynomial3[] y = { new Polynomial3(1.5, 1), 
+							new Polynomial3(2.5, 2), 
+							new Polynomial3(3.3, 3),
+							new Polynomial3(4.0, 0), 
+							new Polynomial3(2.2, 4), 
+							new Polynomial3(3.1, 5), 
+							new Polynomial3(3.8, 6), 
+							};
+		int nx = x.length;
 
-	static void AddPolynomial(Polynomial[] x, Polynomial[] y, Polynomial[] z) {
-		int i = 0;
-		int j = 0;
-		int k = 0;
-		while (i < x.length && j < y.length) {
-			if (x[i].exp == y[j].exp) {
-				z[k++] = new Polynomial(x[i].coef + y[j].coef, x[i].exp);
-				i++;
-				j++;
-			} else if (x[i].exp > y[j].exp) {
-				z[k++] = new Polynomial(x[i].coef, x[i].exp);
-				i++;
-			} else {
-				z[k++] = new Polynomial(y[j].coef, y[j].exp);
-				j++;
-			}
-		}
+		ShowPolynomial("다항식 x = ", x, -1);
+		ShowPolynomial("다항식 y = ", y, -1);
+		MergeSort(x, 0, x.length - 1); // 배열 x를 퀵정렬
+		MergeSort(y, 0, y.length - 1); // 배열 x를 퀵정렬
+		ShowPolynomial("정렬후 다항식 x = ", x, -1);
+		ShowPolynomial("정렬후 다항식 y = ", y, -1);
 
-		while (i < x.length) {
-			z[k++] = new Polynomial(x[i].coef, x[i].exp);
-			i++;
-		}
+		Polynomial3[] z = new Polynomial3[20];
 
-		while (j < y.length) {
-			z[k++] = new Polynomial(y[j].coef, y[j].exp);
-			j++;
-		}
-		MergeSort(z, 0, k - 1);
-	}
+		for (int i = 0; i < z.length; i++)
+			z[i] = new Polynomial3();
 
-	static void ShowPolynomial(Polynomial[] x) {
-		for (int i = 0; i < x.length; i++)
-			System.out.println(x[i].toString());
+		int zTerms = AddPolynomial(x, y, z);// 다항식 덧셈 z = x + y
+		ShowPolynomial("덧셈후 다항식 z = ", z, zTerms);
+
+		zTerms = MultiplyPolynomial(x, y, z);// 다항식 곱셈 z = x * y
+		MergeSort(z, 0, zTerms-1); // 배열 x를 퀵정렬
+		ShowPolynomial("곱셈후 다항식 z = ", z, zTerms);
+		double result = EvaluatePolynomial(z, zTerms, 10);// 다항식 값 계산 함수 z(10) 값 계산한다
+		DecimalFormat formatter = new DecimalFormat("#,###");
+        String formattedNumber = formatter.format(result);
+        System.out.println("result = " + formattedNumber);
 	}
 }
--- ch08/ch08_1.java
+++ ch08/ch08_1.java
@@ -22,31 +22,22 @@ class LinkedList1 {
 	}
 
 	public int Delete(int element) {// delete the element
-	    if (first == null) {
-	        System.out.println("리스트가 비어있습니다.");
-	        return -1;
-	    }
+		Node1 q, current = first;
+		q = current;
 
-	    Node1 p = first;
-	    Node1 q = null;
-
-	    while (p != null) {
-	        if (p.data == element) {
-	            if (q == null) {
-	                first = p.link;
-	            } else {
-	                q.link = p.link;
-	            }
-	            return p.data;
-	        }
-	        q = p;
-	        p = p.link;
-	    }
-
-	    System.out.println("삭제할 데이터가 없습니다.");
-	    return -1;
+		while (current != null) {
+			if (current.data == element) {
+				if (q == first) {
+					first = current.link;
+				} else {
+					q.link = current.link;
+				}
+				return current.data;
+			}
+			current = current.link;
+		}
+		return -1;// 삭제할 대상이 없다.
 	}
-
 
 	public void Show() { // 전체 리스트를 순서대로 출력한다.
 		Node1 p = first;
@@ -57,38 +48,40 @@ class LinkedList1 {
 	}
 
 	public void Add(int element) {// 임의 값을 삽입할 때 리스트가 오름차순으로 정렬이 되도록 한다
-	    Node1 temp = new Node1(element);
+		Node1 newNode = new Node1(element);
+		if (first == null) {
+			first = newNode;
+			return;
+		}
 
-	    if (first == null) {
-	        first = temp;
-	        return;
-	    }
+		if (element <= first.data) {
+			newNode.link = first;
+			first = newNode;
+			return;
+		}
 
-	    if (element <= first.data) {
-	        temp.link = first;
-	        first = temp;
-	        return;
-	    }
+		Node1 p = first;
+		Node1 q = null;
 
-	    Node1 p = first;
-	    Node1 q = null;
-
-	    while (p != null && element > p.data) {
-	        q = p;
-	        p = p.link;
-	    }
-
-	    temp.link = p;
-	    q.link = temp;
+		while (p != null) {
+			if (element > p.data) {
+				q = p;
+				p = p.link;
+			} else {
+				break;
+			}
+		}
+		newNode.link = p;
+		q.link = newNode;
 	}
 
 	public boolean Search(int data) { // 전체 리스트를 순서대로 출력한다.
-		Node1 p = first;
-		while (p != null) {
-			if (p.data == data) {
+		Node1 ptr = first;
+		while (ptr != null) {
+			if (ptr.data == data) {
 				return true;
 			}
-			p = p.link;
+			ptr = ptr.link;
 		}
 		return false;
 	}
@@ -101,14 +94,18 @@ public class ch08_1 {
 		private final String message; // 표시할 문자열
 
 		static Menu MenuAt(int idx) { // 순서가 idx번째인 열거를 반환
-			for (Menu m : Menu.values()) //Add, Delete, Show, Search, Exit
-				if (m.ordinal() == idx)	//ordinal은 index
+			for (Menu m : Menu.values()) // Add,Delete,Show,Search,Exit
+				if (m.ordinal() == idx) // ordinal()은 index값
 					return m;
 			return null;
 		}
 
-		Menu(String string) { // 생성자(constructor)
+		// "Add" 상수가 정의될 때 Menu("삽입") 생성자가 호출되어 message 필드가 "삽입"으로 초기화
+		// 생성자는 각 상수가 정의될 때 호출되며, 해당 상수의 message 필드를 초기화하는 역할
+		// enum 상수가 언제 정의되는가를 찾아 보아야 한다
+		Menu(String string) { // 생성자(constructor)가 언제 호출되는지 파악하는 것이 필요하다
 			message = string;
+			System.out.println("\nMenu 생성자 호출 :: " + message);
 		}
 
 		String getMessage() { // 표시할 문자열을 반환
@@ -121,7 +118,7 @@ public class ch08_1 {
 		Scanner sc = new Scanner(System.in);
 		int key;
 		do {
-			for (Menu m : Menu.values()) {
+			for (Menu m : Menu.values()) { // Menu 생성자 호출
 				System.out.printf("(%d) %s  ", m.ordinal(), m.getMessage());
 				if ((m.ordinal() % 3) == 2 && m.ordinal() != Menu.Exit.ordinal())
 					System.out.println();
@@ -133,39 +130,46 @@ public class ch08_1 {
 	}
 
 	public static void main(String[] args) {
-		Menu menu; // 메뉴
+		Menu menu; // 메뉴 참조 변수일 뿐이다(객체 생성 아님)
 		Random rand = new Random();
-		System.out.println("Linked List");
 		LinkedList1 l = new LinkedList1();
 		Scanner sc = new Scanner(System.in);
 		int data = 0;
-		System.out.println("inserted");
 		l.Show();
 		do {
-			switch (menu = SelectMenu()) {
-			case Add: // 머리노드 삽입
+			switch (menu = SelectMenu()) {// Menu 생성자 호출 - menu 객체를 리턴한다
+			case Add:
 				data = rand.nextInt(20);
-				// double d = Math.random();
-				// data = (int) (d * 50);
 				l.Add(data);
+				System.out.print("삽입 후 리스트 : ");
+				l.Show();
+				System.out.println();
 				break;
-			case Delete: // 머리 노드 삭제
+			case Delete:
 				data = sc.nextInt();
 				int num = l.Delete(data);
-				System.out.println("삭제된 데이터는 " + num);
-				break;
-			case Show: // 꼬리 노드 삭제
+				if (num == -1)
+					System.out.println("삭제할 데이터가 없습니다");
+				else
+					System.out.println("삭제된 데이터는 " + num);
+				System.out.print("삭제 후 리스트 : ");
 				l.Show();
+				System.out.println();
+				break;
+			case Show:
+				System.out.print("리스트 : ");
+				l.Show();
+				System.out.println();
 				break;
 			case Search: // 회원 번호 검색
 				int n = sc.nextInt();
 				boolean result = l.Search(n);
 				if (!result)
-					System.out.println("검색 값 = " + n + "데이터가 없습니다.");
+					System.out.println("검색 값 = " + n + " 데이터가 없습니다.");
 				else
-					System.out.println("검색 값 = " + n + "데이터가 존재합니다.");
+					System.out.println("검색 값 = " + n + " 데이터가 존재합니다.");
 				break;
-			case Exit: // 꼬리 노드 삭제
+			case Exit:
 				break;
 			}
 		} while (menu != Menu.Exit);
